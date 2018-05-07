c7d0d43ea4d8bd3f88a153d1b7beee669494a00b
Jens Yvan De Craecker <jens.de.craecker@outlook.be>
2018-05-07 11:52:14 +0200

    Added abstract filter class with tests

---
+++ src/AdvancedPipeline/Filter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AdvancedPipeline
+{
+    public abstract class Filter<TInput, TOutput> : IFilter<TInput, TOutput>
+    {
+        public abstract TOutput Execute(TInput input);
+
+        public Type InputType { get; } = typeof(TInput);
+
+        public Type OutputType { get; } = typeof(TOutput);
+
+        public Object Execute(Object input)
+        {
+            if (input != null && !(input is TInput)) // Checks if the input is not null and if it is an instance of the desired type.
+                throw new ArgumentException($"The specified input is not an instance of {InputType}.", nameof(input));
+            if (input == null && InputType.IsValueType) // A value type can't be null.
+                throw new ArgumentNullException(nameof(input), $"The specified input can't be null since the input type ({InputType}) is a value type.");
+            return Execute((TInput)input);
+        }
+    }
+}
---
+++ tests/AdvancedPipeline.Tests/FilterTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace AdvancedPipeline.Tests
+{
+    public class FilterTest
+    {
+        private class ConcreteFilter<T> : Filter<T,T>
+        {
+            public override T Execute(T input)
+            {
+                return input;
+            }
+        }
+
+        [Fact]
+        public void ExecuteStructNullArgumentNullExceptionTest()
+        {
+            Assert.Throws<ArgumentNullException>(() => new ConcreteFilter<Int32>().Execute(null));
+        }
+
+        [Fact]
+        public void ExecuteStructTest()
+        {
+           Assert.Equal(0, new ConcreteFilter<Int32>().Execute(0));
+        }
+
+        [Fact]
+        public void ExecuteClassTest()
+        {
+            Assert.Equal("", new ConcreteFilter<String>().Execute(""));
+        }
+
+        [Fact]
+        public void ExecuteClassNullTest()
+        {
+            Assert.Null(new ConcreteFilter<String>().Execute(null));
+        }
+
+        [Fact]
+        public void ExecuteClassDifferentTypeArgumentExceptionTest()
+        {
+            Assert.Throws<ArgumentException>(() => new ConcreteFilter<String>().Execute(new List<Char>()));
+        }
+
+        [Fact]
+        public void ExecuteStructDifferentTypeArgumentExceptionTest()
+        {
+            Assert.Throws<ArgumentException>(() => new ConcreteFilter<Int32>().Execute(true));
+        }
+    }
+}
