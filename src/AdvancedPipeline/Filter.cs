07835367ed332e9f0ca5c7371c66b5c712ef68b2
Jens Yvan De Craecker <jens.de.craecker@outlook.be>
2018-05-07 11:57:29 +0200

    Added summaries

--- src/AdvancedPipeline/Filter.cs
+++ src/AdvancedPipeline/Filter.cs
@@ -2,14 +2,35 @@
 
 namespace AdvancedPipeline
 {
+    /// <summary>
+    ///     Represents an abstract filter that provides basic validation.
+    /// </summary>
+    /// <typeparam name="TInput">The input type of the filter.</typeparam>
+    /// <typeparam name="TOutput">The output type of the filter.</typeparam>
     public abstract class Filter<TInput, TOutput> : IFilter<TInput, TOutput>
     {
+        /// <summary>
+        ///     Executes the current filter with the specified input.
+        /// </summary>
+        /// <param name="input">The input to execute in the current filter.</param>
+        /// <returns>The output of the execution.</returns>
         public abstract TOutput Execute(TInput input);
 
+        /// <summary>
+        ///     Gets the input type that the current filter supports.
+        /// </summary>
         public Type InputType { get; } = typeof(TInput);
 
+        /// <summary>
+        ///     Gets the output type that the current filter supports.
+        /// </summary>
         public Type OutputType { get; } = typeof(TOutput);
 
+        /// <summary>
+        ///     Executes the current filter with the specified input.
+        /// </summary>
+        /// <param name="input">The input to execute in the current filter.</param>
+        /// <returns>The output of the execution.</returns>
         public Object Execute(Object input)
         {
             if (input != null && !(input is TInput)) // Checks if the input is not null and if it is an instance of the desired type.
