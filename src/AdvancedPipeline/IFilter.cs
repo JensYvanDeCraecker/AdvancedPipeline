30e16ed8c90543078f9b05c0c5ab9d4e0bbcf0d8
Jens Yvan De Craecker <jens.de.craecker@outlook.be>
2018-05-07 11:30:22 +0200

    Reformatted the summaries

--- src/AdvancedPipeline/IFilter.cs
+++ src/AdvancedPipeline/IFilter.cs
@@ -3,22 +3,22 @@
 namespace AdvancedPipeline
 {
     /// <summary>
-    /// Represents a filter.
+    ///     Represents a filter.
     /// </summary>
     public interface IFilter
     {
         /// <summary>
-        /// Gets the input type that the current filter supports.
+        ///     Gets the input type that the current filter supports.
         /// </summary>
         Type InputType { get; }
 
         /// <summary>
-        /// Gets the output type that the current filter supports.
+        ///     Gets the output type that the current filter supports.
         /// </summary>
         Type OutputType { get; }
 
         /// <summary>
-        /// Executes the current filter with the specified input.
+        ///     Executes the current filter with the specified input.
         /// </summary>
         /// <param name="input">The input to execute in the current filter.</param>
         /// <returns>The output of the execution.</returns>
@@ -26,14 +26,14 @@ namespace AdvancedPipeline
     }
 
     /// <summary>
-    /// Represents a generic filter.
+    ///     Represents a generic filter.
     /// </summary>
     /// <typeparam name="TInput">The input type of the filter.</typeparam>
     /// <typeparam name="TOutput">The output type of the filter.</typeparam>
     public interface IFilter<in TInput, out TOutput> : IFilter
     {
         /// <summary>
-        /// Executes the current filter with the specified input.
+        ///     Executes the current filter with the specified input.
         /// </summary>
         /// <param name="input">The input to execute in the current filter.</param>
         /// <returns>The output of the execution.</returns>
