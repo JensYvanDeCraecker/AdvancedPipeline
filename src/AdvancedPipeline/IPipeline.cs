d7d56b8fcd242ea2dfce800752e9795319a7314e
Jens Yvan De Craecker <jens.de.craecker@outlook.be>
2018-05-10 01:56:58 +0200

    Added pipeline interface and pipelinestate enum

---
+++ src/AdvancedPipeline/IPipeline.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdvancedPipeline
+{
+    /// <summary>
+    ///     Represents a sequence of filters which can be executed in sequence.
+    /// </summary>
+    public interface IPipeline : IEnumerable<IFilter>
+    {
+        /// <summary>
+        ///     Gets the input type of the current pipeline.
+        /// </summary>
+        Type InputType { get; }
+
+        /// <summary>
+        ///     Gets the output type of the current pipeline.
+        /// </summary>
+        Type OutputType { get; }
+
+        /// <summary>
+        ///     Gets the state of the current pipeline.
+        /// </summary>
+        PipelineState State { get; }
+
+        /// <summary>
+        ///     Gets the output of the last execution of the current pipeline.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">The last execution resulted in an error.</exception>
+        Object Output { get; }
+
+        /// <summary>
+        ///     Gets the error of the last execution of the current pipeline.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">The last execution was successful.</exception>
+        Exception Error { get; }
+
+        /// <summary>
+        ///     Executes the current pipeline with the specified input.
+        /// </summary>
+        /// <param name="input">The input to execute in the current pipeline.</param>
+        /// <returns><c>true</c> if the execution was successful; otherwise, <c>false</c>.</returns>
+        /// <exception cref="InvalidOperationException">The current pipeline is busy.</exception>
+        Boolean Execute(Object input);
+
+        /// <summary>
+        ///     Resets the current pipeline.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">The current pipeline is busy.</exception>
+        void Reset();
+    }
+}
---
+++ src/AdvancedPipeline/PipelineState.cs
@@ -0,0 +1,28 @@
+﻿namespace AdvancedPipeline
+{
+    /// <summary>
+    ///     Represents the state of a pipeline.
+    /// </summary>
+    public enum PipelineState
+    {
+        /// <summary>
+        ///     Indicates that the pipeline hasn't yet been executed.
+        /// </summary>
+        None,
+
+        /// <summary>
+        ///     Indicates that the pipeline is currently being executed.
+        /// </summary>
+        Busy,
+
+        /// <summary>
+        ///     Indicates that the execution of the pipeline was successful.
+        /// </summary>
+        Success,
+
+        /// <summary>
+        ///     Indicates that the last execution of the pipeline resulted in an error.
+        /// </summary>
+        Error
+    }
+}
